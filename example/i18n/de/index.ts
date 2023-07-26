12fc0f34531b3b7fde78c6dc492dd6993eab9a9b
SeanLatimer <sean@latimer.rocks>
2023-07-26 19:06:25 +0100

    inlang: update translations

--- example/i18n/de/index.ts
+++ example/i18n/de/index.ts
@@ -1,14 +1,12 @@
 import type { Translation } from '../i18n-types'
-
 const de: Translation = {
-	// this is an example Translation, just rename or delete this folder if you want
-	HI: 'Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n',
-	PLURAL_FULL: "{{zero|one|two|few|many|other}}",
-	nested: {
-		PLURAL: "Hallo {{Banane|Bananen}}",
-	},
-	schedule: '{0|simpleDate}',
-	spectators: '{0} Zuschauer live',
+   "HI": "Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n",
+   "PLURAL_FULL": "{{zero|one|two|few|many|other}}",
+   "nested": {
+      "PLURAL": "Hallo {{Banane|Bananen}}"
+   },
+   "schedule": "{0|simpleDate}",
+   "spectators": "0 Live-Zuschauer{{s}}"
 }
 
 export default de
--- example/i18n/en/index.ts
+++ example/i18n/en/index.ts
@@ -1,14 +1,12 @@
 import type { BaseTranslation } from '../i18n-types'
-
 const en: BaseTranslation = {
-	// TODO: your translations go here
-	HI: 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
-	PLURAL_FULL: "{{zero|one|two|few|many|other}}",
-	nested: {
-		PLURAL: "hello banana{{s}}",
-	},
-	schedule: '{0:Date|simpleDate}',
-	spectators: '{0} live spectator{{s}}',
+   "HI": "Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n",
+   "PLURAL_FULL": "{{zero|one|two|few|many|other}}",
+   "nested": {
+      "PLURAL": "hello banana{{s}}"
+   },
+   "schedule": "{0:Date|simpleDate}",
+   "spectators": "{0} live spectator{{s}}"
 }
 
 export default en
